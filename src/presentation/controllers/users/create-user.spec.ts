9cf90d622e523a461e9bdbfeb1304f96a0e0db0e
Maykon A. Oliveira <moliveira.anschau@gmail.com>
2021-12-16 23:04:05 -0300

    test: ensure return 500 if CreateUser throw error

--- src/presentation/controllers/users/create-user.spec.ts
+++ src/presentation/controllers/users/create-user.spec.ts
@@ -2,6 +2,7 @@ import { CreateUserController } from './create-user'
 import { HttpRequest } from '../../protocols'
 import { CreateUser, CreateUserDTO } from '../../../domain/usecases/create-user'
 import { UserModel } from '../../../domain/models/user'
+import { serverError } from '../../helpers/http'
 
 class CreateUserStub implements CreateUser {
   async execute (data: CreateUserDTO): Promise<UserModel> {
@@ -25,21 +26,34 @@ const makeSut = (): SutType => {
   }
 }
 
+const makeFakeUser = (): CreateUserDTO => ({
+  name: 'foo',
+  email: 'foo@example.com',
+  cpf: 'foo_cpf',
+  password: 'password',
+  admin: false
+})
+
 describe('Create User Controller', () => {
   test('should call CreateUser with correct values', async () => {
     const { sut, createUserStub } = makeSut()
     const executeSpy = jest.spyOn(createUserStub, 'execute')
 
-    const user = {
-      name: 'foo',
-      email: 'foo@example.com',
-      cpf: 'foo_cpf',
-      password: 'password'
-    }
-
+    const user = makeFakeUser()
     const httpRequest: HttpRequest = { body: user }
     await sut.handle(httpRequest)
 
     expect(executeSpy).toHaveBeenCalledWith({ ...user, admin: false })
+  })
+
+  test('should return 500 if CreateUser throws error', async () => {
+    const { sut, createUserStub } = makeSut()
+    jest.spyOn(createUserStub, 'execute').mockImplementationOnce(async () => {
+      return await new Promise((resolve, reject) => { reject(new Error('')) })
+    })
+
+    const httpResponse = await sut.handle({ body: makeFakeUser() })
+
+    expect(httpResponse).toEqual(serverError(new Error('')))
   })
 })
